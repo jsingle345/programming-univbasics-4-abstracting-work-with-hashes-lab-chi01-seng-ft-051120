1de8932992a9e7bd8bfef7358fb2d27bee8135c9
Joshua Singleton <jsingle345@gmail.com>
2020-04-30 18:50:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,5 @@
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
-  {name: "Joshua"}
+  {name: "Joshua Singleton"}
 end
 
 def read_from_hash(hash, key)
